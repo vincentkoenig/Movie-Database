fdc2dd6b7e2c45ef928dba3fec753d25852453d7
Vincent König <vincentkoenig97@gmail.com>
2026-04-17 20:27:56 +0200

    new imports

---
+++ movie_api.py
@@ -0,0 +1,24 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+API_URL = "http://www.omdbapi.com/"
+
+def fetch_movie_data(title):
+    """Ruft Filmdaten von der OMDb API ab."""
+    response = requests.get(API_URL, params={"apikey": API_KEY, "t": title})
+    data = response.json()
+
+    if data.get("Response") == "False":
+        print(f"Movie '{title}' not found on OMDb!")
+        return None
+
+    return {
+        "title": data["Title"],
+        "year": int(data["Year"]),
+        "rating": float(data["imdbRating"]),
+        "poster": data["Poster"]
+    }
